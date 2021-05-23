da966833833b3e1591cb2acd017e821e69f9ce3f
wen <wen@feng.com>
2021-05-23 09:37:15 +0800

    String的 方式一:通过字面量定义的方式

---
+++ CommonClasses/src/com/feng/java/StringTest.java
@@ -0,0 +1,13 @@
+package com.feng.java;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println(s1==s2);
+        s1 = "hello";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1==s2);
+    }
+}
