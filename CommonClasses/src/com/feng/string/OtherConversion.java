65d4d7d9d67e2eef87d95049d8e331c08214f04f
wen <wen@feng.com>
2021-05-24 13:40:23 +0800

    byte类型数组和String类型之间的转换，涉及到格式（utf-8、gbk)

--- CommonClasses/src/com/feng/string/OtherConversion.java
+++ CommonClasses/src/com/feng/string/OtherConversion.java
@@ -1,7 +1,18 @@
 package com.feng.string;
 
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class OtherConversion {
-    public static void main(String[] args) {
+
+    /**
+     * String--char[]
+     */
+    @Test
+    public void test1() {
         String s1 = "abcdefghijklmnopqrstuvwxyz";
         char[] arr = s1.toCharArray();
         System.out.println(s1);
@@ -20,5 +31,34 @@ public class OtherConversion {
         System.out.println(s3);
         System.out.println("------------------------ ");
 //        char[] arr4 = {"abc"};
+    }
+
+    /**
+     * String--byte[]
+     */
+    @Test
+    public void test2() throws UnsupportedEncodingException {
+        String s1 = "123abc";
+        byte[] b1 = s1.getBytes();
+        System.out.println(Arrays.toString(b1));
+
+        System.out.println("----------------");
+
+        String s2 = "中国";
+        byte[] b2 = s2.getBytes();
+        System.out.println(Arrays.toString(b2));
+
+        System.out.println("---------------");
+
+        String s3 = "I Love You, 中国";
+        byte[] b3 = s3.getBytes("gbk");
+        System.out.println(Arrays.toString(b3));
+
+        System.out.println("---------------");
+
+        String s4_last = new String(b3);//使用utf-8创建（解码）byte数组（之前是gbk格式的）会乱码
+        System.out.println(s4_last);
+        String s4 = new String(b3,"gbk");
+        System.out.println(s4);
     }
 }
