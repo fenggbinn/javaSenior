8de0b91062fbc418f67fad5dd96ba88d17e58753
wen <wen@feng.com>
2021-05-27 10:54:10 +0800

    Calendar的get()方法

--- CommonClasses/src/com/feng/other/CalendarTest1.java
+++ CommonClasses/src/com/feng/other/CalendarTest1.java
@@ -10,4 +10,19 @@ public class CalendarTest1 {
         Calendar  calendar = Calendar.getInstance();
         System.out.println(calendar.getClass());
     }
+
+    /**
+     * Calendar的get()方法
+     */
+    @Test
+    public void test2(){
+        Calendar  calendar = Calendar.getInstance();
+        System.out.println(calendar.getClass());
+        int days1 = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days1);
+        int days2 = calendar.get(Calendar.DAY_OF_WEEK);
+        System.out.println(days2);
+        int days3 = calendar.get(Calendar.DAY_OF_YEAR);
+        System.out.println(days3);
+    }
 }
