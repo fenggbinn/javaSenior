c85284a7642962facd5ea5739368d2d51398879a
wen <wen@feng.com>
2021-05-27 16:36:30 +0800

    //获取自1970年1月1日日时e分日秒(UTC)开始的毫秒数         long e = instant.toEpochMilli();

--- CommonClasses/src/com/feng/newDate/InstantTest.java
+++ CommonClasses/src/com/feng/newDate/InstantTest.java
@@ -16,4 +16,14 @@ public class InstantTest {
         OffsetDateTime offsetDateTime = instant.atOffset(ZoneOffset.ofHours(8));
         System.out.println(offsetDateTime);
     }
+
+    @Test
+    public void test2(){
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        //获取自1970年1月1日日时e分日秒(UTC)开始的毫秒数
+        long e = instant.toEpochMilli();
+        System.out.println(e);
+    }
 }
