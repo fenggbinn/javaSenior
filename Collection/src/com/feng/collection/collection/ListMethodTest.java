de09a344763cced1709f2581fe0121835904b7f4
wen <wen@feng.com>
2021-05-29 20:45:26 +0800

    * get()      * remove()      * indexOf()      * lastIndexOf()      * set()      * subList()

--- Collection/src/com/feng/collection/collection/ListMethodTest.java
+++ Collection/src/com/feng/collection/collection/ListMethodTest.java
@@ -2,9 +2,15 @@ package com.feng.collection.collection;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListMethodTest {
+
+    /**
+     * add()和addAll()方法
+     */
     @Test
     public void test1(){
 //        System.out.println("test1");
@@ -31,5 +37,60 @@ public class ListMethodTest {
         a1.remove(a2);
         System.out.println(a1);
 
+    }
+
+    /**
+     * get()
+     * remove()
+     * indexOf()
+     * lastIndexOf()
+     * set()
+     * subList()
+     */
+    @Test
+    public void test2(){
+        ArrayList a1 = new ArrayList();
+        a1.add("今天");
+        a1.add("星期");
+        a1.add("六");
+        System.out.println(a1);
+
+        System.out.println("--------get()----------");
+        System.out.println(a1.get(1));
+        System.out.println(a1.get(2));
+//        System.out.println(a1.get(5));//java.lang.IndexOutOfBoundsException: Index: 5, Size: 3
+
+        System.out.println("--------remove()----------");
+        a1.remove("今天");
+        System.out.println(a1);
+        a1.remove(1);
+        System.out.println(a1);
+
+
+        System.out.println("--------indexOf()----------");
+        //返回值第一次出现的下标
+        a1.add("六");
+        a1.add("六");
+        a1.add("六");
+        System.out.println(a1);
+        System.out.println(a1.indexOf(1));
+        System.out.println(a1.indexOf("六"));
+
+        System.out.println("--------lastIndexOf()----------");
+        System.out.println(a1);
+        System.out.println(a1.lastIndexOf("六"));
+        System.out.println(a1.lastIndexOf("9"));
+
+
+        System.out.println("--------set()----------");
+        System.out.println(a1);
+        a1.set(0,"六");
+        System.out.println(a1);
+
+        System.out.println("--------subList()----------");
+        System.out.println(a1);
+        List s = a1.subList(0, 3);//左闭右开[a,b)
+        System.out.println(s);
+        System.out.println(a1);//原来的ArrayList不变，直接产生一个新的子集合
     }
 }
