46ded64caa91b99a01e6318ae2293726a26286fd
wen <wen@feng.com>
2021-05-29 18:44:42 +0800

    iterator.remove();

--- Collection/src/com/feng/collection/collection/IteratorTest.java
+++ Collection/src/com/feng/collection/collection/IteratorTest.java
@@ -21,4 +21,27 @@ public class IteratorTest {
             System.out.println(iterator.next());
         }
     }
+
+    @Test
+    public void test2(){
+        Collection col = new ArrayList();
+        col.add("1");
+        col.add("2");
+        col.add("3");
+        System.out.println(col);
+        System.out.println("-----------------");
+
+        Iterator iterator = col.iterator();
+        while (iterator.hasNext()){
+            if (iterator.next()=="3"){
+                iterator.remove();
+            }
+//            System.out.println(iterator.next());
+        }
+        //上面while循环迭代器已经遍历到末尾了，需要重新赋值
+        iterator = col.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
 }
