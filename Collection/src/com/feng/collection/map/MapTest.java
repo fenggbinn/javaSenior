a87f73c880243c7526f38bbb8e8b218411c40d0a
wen <wen@feng.com>
2021-05-30 18:41:46 +0800

    HashMap的使用，map = new LinkedHashMap();

--- Collection/src/com/feng/collection/map/MapTest.java
+++ Collection/src/com/feng/collection/map/MapTest.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapTest {
     @Test
     public void test1(){
         Map map = new HashMap();
+        map = new LinkedHashMap();
         map.put(null,null);
         System.out.println(map);
 
@@ -17,5 +19,11 @@ public class MapTest {
         /*Hashtable hashtable = new Hashtable();//java.lang.NullPointerException
         hashtable.put(null,null);
         System.out.println(hashtable);*/
+
+        System.out.println("------------");
+        map.put(1,'a');
+        map.put(2,'b');
+        map.put(3,'c');
+        System.out.println(map);
     }
 }
