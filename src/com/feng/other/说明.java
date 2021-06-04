049dd9bca736226486d144a4b2b4c9daa034c0f8
wen <wen@feng.com>
2021-06-04 14:21:41 +0800

    说明: 在IDEA中: 如果大家开发使用JUnit中的单元测试方法测试，相对路径即为当前Module下。 如果大家使用main(测试，相对路径即为当前的Project下。 Eclipse中: 不管使用单元测试方法还是使用mainO测试，相对路径都是当前的Project下。

---
+++ src/com/feng/other/说明.java
@@ -0,0 +1,12 @@
+package com.feng.other;
+
+public class 说明 {
+    /**
+     * 说明:
+     * IDEA中:
+     * 如果大家开发使用JUnit中的单元测试方法测试，相对路径即为当前Module下。
+     * 如果大家使用main(测试，相对路径即为当前的Project下。
+     * Eclipse中:
+     * 不管使用单元测试方法还是使用mainO测试，相对路径都是当前的Project下。
+     */
+}
