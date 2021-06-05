50fc71e12974d550dbec2b87d4b92998b8a474b8
wen <wen@feng.com>
2021-06-05 21:31:24 +0800

    //解密图片代码，和上面加密代码一样，异或回去了

--- IOStream/src/com/feng/fileio/EncryptionPicture.java
+++ IOStream/src/com/feng/fileio/EncryptionPicture.java
@@ -19,7 +19,7 @@ public class EncryptionPicture {
 
             while ((len=bi.read(bytes))!=-1){
                 for (int i = 0; i < len; i++) {
-                    bytes[i] = (byte)(bytes[i]^5);
+                    bytes[i] = (byte)(bytes[i]^5);//加密代码
                 }
                 bo.write(bytes,0,len);
             }
@@ -41,7 +41,43 @@ public class EncryptionPicture {
                 }
             }
         }
+    }
 
+    //解密图片
+    @Test
+    public void test2(){
+        BufferedInputStream bi = null;
+        BufferedOutputStream bo = null;
+        try {
+            bi = new BufferedInputStream(new FileInputStream(new File("security.png")));
+            bo = new BufferedOutputStream(new FileOutputStream(new File("decrypt.png")));
 
+            byte[] bytes = new byte[1024];
+            int len;
+
+            while ((len=bi.read(bytes))!=-1){
+                for (int i = 0; i < len; i++) {
+                    bytes[i] = (byte)(bytes[i]^5);//解密代码，和上面加密代码一样，异或回去了
+                }
+                bo.write(bytes,0,len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bi!=null){
+                try {
+                    bi.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bo!=null){
+                try {
+                    bo.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
