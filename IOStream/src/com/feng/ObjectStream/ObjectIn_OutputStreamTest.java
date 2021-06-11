18f02a261a400485ac74cba396de63399e66fcdb
wen <wen@feng.com>
2021-06-11 09:14:04 +0800

    test对象流的读取方式：java.lang.ClassCastException: java.lang.String cannot be cast to com.feng.ObjectStream.Person

--- IOStream/src/com/feng/ObjectStream/ObjectIn_OutputStreamTest.java
+++ IOStream/src/com/feng/ObjectStream/ObjectIn_OutputStreamTest.java
@@ -19,6 +19,7 @@ public class ObjectIn_OutputStreamTest {
 //            }
             oos.writeObject(new String("kdf看看"));
             oos.writeObject(new Person(101,"留白",99,"女"));//java.io.NotSerializableException: com.feng.ObjectStream.Person
+            oos.writeObject(new String("test对象流的读取方式"));
             oos.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,14 +44,20 @@ public class ObjectIn_OutputStreamTest {
             ois = new ObjectInputStream(new FileInputStream("oos.dat"));
             Object o = null;
             Person person=null;
+            Person person2=null;
             try {
                 o = ois.readObject();
                 person = (Person)ois.readObject();
+                /**
+                 * java.lang.ClassCastException: java.lang.String cannot be cast to com.feng.ObjectStream.Person
+                 */
+                person2 = (Person)ois.readObject();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
             System.out.println((String)o);
             System.out.println(person);
+            System.out.println(person2);
             /*int len;
             byte[] bytes = new byte[10];
             while ((len=ois.read(bytes))!=-1){
