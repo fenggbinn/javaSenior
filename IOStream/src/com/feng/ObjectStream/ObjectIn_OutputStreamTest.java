8b0491c3eb460a94e2a1f1a73541caad5236a3ad
wen <wen@feng.com>
2021-06-11 09:20:25 +0800

    test对象流的读取方式：ObjectInputSteam读取文件的时候根据 .dat 文件一行一行的读取，类型要确定，不能把String类型的使用Person类型读取文件： String str = (String)ois.readObject(); Person person = (Person)ois.readObject(); 文件写入dat的时候是按照对应的方式写入的：  oos.writeObject(new String("kdf看看"));             oos.writeObject(new Person(101,"留白",99,"女"));//java.io.NotSerializableException: com.feng.ObjectStream.Person             oos.writeObject(new String("test对象流的读取方式"));

--- IOStream/src/com/feng/ObjectStream/ObjectIn_OutputStreamTest.java
+++ IOStream/src/com/feng/ObjectStream/ObjectIn_OutputStreamTest.java
@@ -44,20 +44,28 @@ public class ObjectIn_OutputStreamTest {
             ois = new ObjectInputStream(new FileInputStream("oos.dat"));
             Object o = null;
             Person person=null;
-            Person person2=null;
+            //Person person2=null;
+            String str=null;
             try {
                 o = ois.readObject();
                 person = (Person)ois.readObject();
                 /**
                  * java.lang.ClassCastException: java.lang.String cannot be cast to com.feng.ObjectStream.Person
                  */
-                person2 = (Person)ois.readObject();
+                //person2 = (Person)ois.readObject();
+                /**
+                 * kdf看看
+                 * Person{id=101, name='留白', age=99, gender='女'}
+                 * test对象流的读取方式
+                 */
+                str = (String)ois.readObject();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
             System.out.println((String)o);
             System.out.println(person);
-            System.out.println(person2);
+           // System.out.println(person2);
+            System.out.println(str);
             /*int len;
             byte[] bytes = new byte[10];
             while ((len=ois.read(bytes))!=-1){
