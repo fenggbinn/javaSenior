1eefaff227f73f7b58ece926a959fe2f46c52230
wen <wen@feng.com>
2021-06-15 10:49:00 +0800

    以FileInputStream的方式加载properties对象的时候，如果properties文件对象在src下，可以使用此方式。建议把properties文件放在src目录下（也不一定）

--- Reflection/src/com/feng/test/PropertiesTest.java
+++ Reflection/src/com/feng/test/PropertiesTest.java
@@ -10,12 +10,13 @@ public class PropertiesTest {
     @Test
     public void test1() throws Exception{
         Properties pros = new Properties();
-        FileInputStream fin = new FileInputStream("test.properties");
+//        FileInputStream fin = new FileInputStream("test.properties");
+        FileInputStream fin = new FileInputStream("src/testSrc.properties");
         pros.load(fin);
         String name = pros.getProperty("name");
         System.out.println(name);
-        String age = pros.getProperty("age");
-        System.out.println(age);
+//        String age = pros.getProperty("age");
+//        System.out.println(age);
     }
 
     @Test
