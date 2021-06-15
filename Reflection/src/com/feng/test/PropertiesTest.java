12aede8682832292eccc5193a3fb930aaafac8db
wen <wen@feng.com>
2021-06-15 10:45:38 +0800

    Properties pros = new Properties();         //类加载器的方式         //获取当前类的构造器         ClassLoader classLoader = PropertiesTest.class.getClassLoader();         //读取相应的文件为输入流的方式         InputStream in = classLoader.getResourceAsStream("testSrc.properties");         //properties文件加载         pros.load(in);

--- Reflection/src/com/feng/test/PropertiesTest.java
+++ Reflection/src/com/feng/test/PropertiesTest.java
@@ -3,6 +3,7 @@ package com.feng.test;
 import org.junit.Test;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesTest {
@@ -15,5 +16,21 @@ public class PropertiesTest {
         System.out.println(name);
         String age = pros.getProperty("age");
         System.out.println(age);
+    }
+
+    @Test
+    public void test2() throws Exception{
+        Properties pros = new Properties();
+        //类加载器的方式
+        //获取当前类的构造器
+        ClassLoader classLoader = PropertiesTest.class.getClassLoader();
+        //读取相应的文件为输入流的方式
+        InputStream in = classLoader.getResourceAsStream("testSrc.properties");
+        //properties文件加载
+        pros.load(in);
+        String name = pros.getProperty("name");
+        System.out.println(name);
+        String date = pros.getProperty("date");
+        System.out.println(date);
     }
 }
