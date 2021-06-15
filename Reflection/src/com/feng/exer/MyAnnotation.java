b76c2e1b44ba89bc339979262531d7bae2c07ad1
wen <wen@feng.com>
2021-06-16 07:43:36 +0800

    @MyAnnotation(value = "tom")//这个之前不能用注解的原因的我自定义的注解类上面的权限没有打开这个上面能加注解，打开权限（添加相应的权限）就可以添加了如下：

--- Reflection/src/com/feng/exer/MyAnnotation.java
+++ Reflection/src/com/feng/exer/MyAnnotation.java
@@ -5,10 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.*;
 
-@Target({TYPE, FIELD})
+@Target({TYPE, FIELD,METHOD,PARAMETER,CONSTRUCTOR,LOCAL_VARIABLE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
     String value() default "hello";
--- Reflection/src/com/feng/exer/Person.java
+++ Reflection/src/com/feng/exer/Person.java
@@ -11,7 +11,8 @@ public class Person extends Creature<String> implements Comparable<String> ,MyIn
 
     }
 
-//    @MyAnnotation(value = "tom")
+//    @MyAnnotation(value = "tom")//这个之前不能用注解的原因的我自定义的注解类上面的权限没有打开这个上面能加注解，打开权限（添加相应的权限）就可以添加了如下：
+    @MyAnnotation(value = "tom")
     private Person(String name){
         this.name=name;
     }
@@ -25,7 +26,7 @@ public class Person extends Creature<String> implements Comparable<String> ,MyIn
         return interests;
     }
 
-//    @MyAnnotation
+//    @MyAnnotation//同上
     private String show(String nation){
         System.out.println("我的国籍是"+nation);
         return nation;
