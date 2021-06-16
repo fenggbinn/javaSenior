972c9cb71c7759b19106f5cc2e7ed2fbba62fdf9
wen <wen@feng.com>
2021-06-16 11:57:38 +0800

    调用构造器创建对象的方式：Constructor<Person> con = clazz.getDeclaredConstructor();         con.setAccessible(true);

--- Reflection/src/com/feng/exer2/ReflectionTest.java
+++ Reflection/src/com/feng/exer2/ReflectionTest.java
@@ -3,6 +3,7 @@ package com.feng.exer2;
 import com.feng.exer.Person;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -26,5 +27,27 @@ public class ReflectionTest {
         show.setAccessible(true);
         Object china = show.invoke(person, "China");
         System.out.println(china);
+    }
+
+    @Test
+    public void testStaticMethod() throws Exception{
+        Class<Person> clazz = Person.class;
+        Person person = clazz.newInstance();
+        Method show = clazz.getDeclaredMethod("show2", String.class);
+        show.setAccessible(true);
+        Object china = show.invoke(person, "China");
+        System.out.println(china);
+    }
+
+
+    @Test
+    public void testUseConstruct()throws Exception{
+        Class<Person> clazz = Person.class;
+//        Constructor<Person> con = clazz.getDeclaredConstructor(String.class);//指定newInstance()方法的时候是传入什么参数
+        Constructor<Person> con = clazz.getDeclaredConstructor();
+        con.setAccessible(true);//这个不要忘记设置了
+//        Person person = con.newInstance("tom");
+        Person person = con.newInstance();
+        System.out.println(person);
     }
 }
